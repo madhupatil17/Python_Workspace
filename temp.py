1ce528ae468f4d125c2292d52ba92df0fc06ab71
Madhusudan Patil <study.madhusudanpatil@gmail.com>
2023-03-25 18:24:43 +0530

    test commit

--- temp.py
+++ temp.py
@@ -1,18 +1,68 @@
-# from turtle import Turtle, Screen
+# inputObj = 'B78$5$d90m326u6799A5c67r21i@p#8t' #B78$5$d90m326u6  A5c67r21i@p#8t
+# outObj = ''
+# listOfSpChars = list()
+# listOfOdd = list()
+# listOfEven = list()
+# finalList = list()
 #
-# turtle_object = Turtle()
-# screen = Screen()
-# turtle_object.turtlesize(2)
-# turtle_object.tilt(-45)
-# screen.exitonclick()
+# for _ in inputObj:
+#     if not _.isalnum():
+#         listOfSpChars.append(_)
+#     if _.isnumeric() and int(_) % 2 == 0:
+#         listOfEven.append(_)
+#     elif _.isnumeric() and not int(_) % 2 == 0:
+#         listOfOdd.append(_)
+#
+# largestList = 0
+# if len(listOfEven) > len(listOfOdd):
+#     largestList = len(listOfEven)
+# else:
+#     largestList = len(listOfOdd)
+#
+# def appendAlternate(list1, list2, largelist):
+#     for i in range(0, largestList):
+#         try:
+#             finalList.append(list1[i])
+#         except:
+#             pass
+#         try:
+#             finalList.append(list2[i])
+#         except:
+#             pass
+#
+# if len(listOfSpChars) % 2 == 0:
+#     appendAlternate(listOfEven, listOfOdd, largestList)
+# else:
+#     appendAlternate(listOfOdd, listOfEven, largestList)
+#
+# for _ in finalList:
+#     outObj += _
+#
+# print(inputObj)
+# print(listOfOdd, listOfEven)
+# print(outObj)
 
-ll = [1,2,3,4,6,8,9,0]
-values = {}
-print(*enumerate(ll, 0))
 
+import random
+import math
+list = ["Sandipan", "Naresh", "Madhusudan", "Shilpashree", "Mouna", "Arati"]
+listA = []
+listB = []
 
+for _ in range(0, len(list)):
 
+    choicee1 = random.choice(list)
+    choicee2 = random.choice(list)
+    if not choicee1.__eq__(choicee2):
+        if choicee1 not in listA and choicee2 not in listA:
+            listA.append(choicee1)
+        if choicee2 not in listB and choicee1 not in listB:
+            listB.append(choicee2)
+    else:
+        continue
 
+for j in range(0, len(listA)):
+    print(listA[j], " : ", listB[j])
 
 
 
